322b4c6485fed0070461071227526541accfd0f1
RIYA5561 <agrawalriya868@gmail.com>
2025-09-02 20:57:41 +0530

    Initial commit

---
+++ app.java
@@ -0,0 +1,21 @@
+public class app {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        System.out.print("Reversed array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
